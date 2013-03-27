110736d430653f2a9805b44ab9737a9efd687498
AustP <austenprobst@gmail.com>
2013-03-27 13:58:19 -0600

    Added error if key is bad

--- session.io.js
+++ session.io.js
@@ -7,14 +7,17 @@ module.exports = function(cookieParser, sessionStore, key, fn){
 			fn? fn(handshake, done): done(err, bool);
 		}
 
-		if(!handshake || !handshake.headers || !handshake.headers.cookie) next('No cookie', false);
+		if(!handshake || !handshake.headers || !handshake.headers.cookie) next('No cookie in header', false);
 		cookieParser(handshake, {}, function(err){
 			if(err) next(err, false);
+			
 			var session_id = (handshake.secureCookies && handshake.secureCookies[key])
 				|| (handshake.signedCookies && handshake.signedCookies[key])
 				|| (handshake.cookies && handshake.cookies[key]);
+			if(!session_id) next('Could not find cookie with key: ' + key, false);
+			
 			sessionStore.load(session_id, function(err, session){
-			if(err) next(err, false);
+				if(err) next(err, false);
 				handshake.session = session;
 				next(null, true);
 			});
