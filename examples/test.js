04c86ac6b82e4587408745cd5b9eb8eaaa9156af
AustP <AustP>
2013-03-27 13:42:07 -0600

    Added example

---
+++ examples/test.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+//Setup cookie and session handlers
+//Note: for sessionStore you can use any sessionStore module that has the .load() function
+//but I personally use the module 'sessionstore' to handle my sessionStores.
+var cookieParser = express.cookieParser('secret');
+var sessionStore = require('sessionstore').createSessionStore();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  //...truncate...//
+  app.use(cookieParser);
+  app.use(express.session({secret: 'secret', store: sessionStore}));
+  app.use(app.router);
+});
+
+app.get('/', function(req, res){
+  res.send('<script src="/socket.io/socket.io.js"></script><script>io.connect();</script>Connected');
+});
+
+server.listen(app.get('port'), function(){
+  console.log('Listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.configure(function(){
+  //use session.io to get our session data
+  io.set('authorization', require('session.io')(cookieParser, sessionStore));
+});
+
+io.on('connection', function(socket){
+  //we now have access to our session data like so
+  var session = socket.handshake.session;
+  console.log(session);
+});
